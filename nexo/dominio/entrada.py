1fc1f7d6567d4875f5ffc3c5e11b253287d7d9c9
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-15 21:39:35 -0300

    feat(entrada): implementa script base da classe entrada que representa o valor creditado e suas abstrações

--- nexo/dominio/entrada.py
+++ nexo/dominio/entrada.py
@@ -3,6 +3,21 @@ from datetime import date
 from .movimentacao import Movimentacao
 
 class Entrada(Movimentacao):
+    def __init__(
+        self,
+        descricao,
+        valor,
+        data,
+        conta,
+        categoria
+    ):
+        super().__init__(
+            descricao,
+            valor,
+            data,
+            conta,
+            categoria
+        ):
 
     def impacto(self):
         return self.valor
