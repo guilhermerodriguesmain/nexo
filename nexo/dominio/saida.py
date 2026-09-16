9797366702629e0ac4b87717357cbd08193163ea
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-15 21:56:23 -0300

    refactor(saida): implementa melhorias nas lógicas de tipos d edebitos

--- nexo/dominio/saida.py
+++ nexo/dominio/saida.py
@@ -7,13 +7,13 @@ class Saida(Movimentacao):
     #sugestão: adicionar lógica para retornar n° parcelas contando que a primeira parcela é ja no mês atual portanto num_parcelas : num_parcelas -1 ou podemos tratar em parcela.py quando podemos fazer a verificação de datetime para saber qual o mes atual e implentar logica de parcelas restantes
     def __init__(
         self,
-        descricao = None,
+        descricao,
         valor,
         data,
         conta,
-        categoria
-        num_parcelas = 1
-        valor_parcelas 
+        categoria,
+        num_parcelas = 1,
+        valor_parcela = none
     ):
         super().__init__(
             descricao,
@@ -21,33 +21,35 @@ class Saida(Movimentacao):
             data,
             conta,
             categoria
-        ):
+        )
 
-        self.num_parcelas = num_parcelas
-        self.valor_parcelas = valor_parcelas
+        self.num_parcelas = num_parcela
+        self.valor_parcela = valor_parcela
+
+        if (valor_parcela is None) and (num_parcelas == 1):
+            # compra a vista
+            self.valor_parcela = valor
+        elif (valor_parcela is None) and (num_parcelas > 1):
+            # calcular parcelas a partir do valor total
+            self._parcelar_por_valor(valor, num_parcelas)
+        else:
+            # tenho valor da parcela e quero calcular o valor total a partir do valor das parcelas e sua quantidade
+            self._parcelar_por_quantidade(valor_parcela, num_parcelas)
+
 
     def impacto(self):
         return - self.valor
 
 
-    def parcelar_por_valor(self, valor_total:float, num_parcelas:int = 1):
+    def _parcelar_por_valor(self, valor_total:float, num_parcelas:int = 1):
         # usar quando por ex: 500 divididos em 5 parcelas
+        self.valor_parcela =  valor_total / num_parcelas
         self.num_parcelas = num_parcelas
         self.valor = valor_total
 
-        if num_parcelas > 1 and (self.valor_parcela is None):
-             self.valor_parcelas =  valor_total / num_parcelas
-        else:
-             self.valor_parcelas =  valor_total 
-
-
-    def parcelar_por_quantidade(self, valor_parcelas:float, num_parcelas:int = 1):
+    def _parcelar_por_quantidade(self, valor_parcela:float, num_parcelas:int = 1):
         #usar quando por ex: 5 parcelas de 100
-        
-        self.valor_parcelas =  valor_parcelas 
+        self.valor_parcela =  valor_parcela 
         self.num_parcelas = num_parcelas
+        self.valor = valor_parcela * num_parcelas
         
-        if num_parcelas > 1 :
-             self.valor = valor_parcelas * num_parcelas
-        else:
-             self.valor = valor_parcelas
