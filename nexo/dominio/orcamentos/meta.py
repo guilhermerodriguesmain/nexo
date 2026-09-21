b3b571c0d7ad56565f83ad51c5fbd2330976484f
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-20 21:31:13 -0300

    featr(acumulo): -implementa métodos valor, calculo_mensal e, calculo_anual - implementa estrutura inicial da classe acumulo

--- nexo/dominio/orcamentos/meta.py
+++ nexo/dominio/orcamentos/meta.py
@@ -1,6 +1,32 @@
 from .orcamento import Orcamento
 
 class Acumulo(Orcamento):
-    def __init__():
-        super().__init__:
+    def __init__(
+        self,
+        data_registro,
+        conta,
+        categoria,
+        descricao,
+        periodicidade,
+        valor_meta
+    ):
+        super()__init__(
+            data_registro,
+            conta,
+            categoria,
+            descricao,
+            periodicidade
+        ):
+
+       self.valor_meta = valor_meta
+
+    @property
+    def valor(self):
+        return self.valor_meta
+
+    def calculo_mensal():
+        return super().calculo_mensal()
+
+    def calculo_anual():
+        return super().calculo_anual()
         
