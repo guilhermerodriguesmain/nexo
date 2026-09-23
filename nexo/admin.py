dd780aaf753b5e4cb8ee2bb2aca2a28b168d9b46
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-23 01:31:13 -0300

    - implementa ação de registrar movimentação financeira de entrada em admin

--- nexo/admin.py
+++ nexo/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from nexo.models import Entrada
+from nexo.services.movimentacoes import MovimentacaoService
+
+
+@admin.register(Entrada)
+class EntradaAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "descricao",
+        "valor",
+        "data_registro",
+        "conta",
+        "categoria",
+    )
+
+    def save_model(self, request, obj, form, change):
+        dados = {
+            "descricao": obj.descricao,
+            "valor": obj.valor,
+            "data_registro": obj.data_registro,
+            "conta": obj.conta,
+            "categoria": obj.categoria,
+        }
+
+        MovimentacaoService().registrar_entrada(dados)
