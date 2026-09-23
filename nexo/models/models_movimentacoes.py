0556412326e7bca90481f7c22cd5617fd67b9547
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-23 00:02:39 -0300

    - implementa models entrada, saida e parcela

--- nexo/models/models_movimentacoes.py
+++ nexo/models/models_movimentacoes.py
@@ -2,11 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Movimetacao(models.Model):
-    valor = models.DecimalField(max_digits= 12, decimal_place = 2)
-    data_registro = models.DateField()
-    conta = models.CharField(max_legth= 100)
-    categoria = models.CharField(max_length = 200)
-    descricao = models.TextField()
+    valor = models.DecimalField(
+        max_digits= 12, 
+        decimal_place = 2
+        null= False
+        blank = False
+    )
+    data_registro = models.DateField(
+        null = False, 
+        blank = False
+    )
+    conta = models.CharField(
+        max_legth= 100,
+        null = False,
+        blank = False
+    )
+    categoria = models.CharField(
+        max_length = 200,
+        null = False,
+        Blank = False
+    )
+    descricao = models.TextField(
+        null = True,
+        blank = True
+    )
     class Meta:
         abstract = True
 
@@ -14,7 +33,21 @@ class Entrada(Movimentacao):
     pass
 
 class Saida(Movimentacao):
-    pass
+    num_parcelas = models.PositiveIntegerField(default=1)
+    valor_parcela = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    
 
-class Parcela(Saida):
-    pass
+class Parcela(Movimentacao):
+    saida = models.ForeignKey(
+        Saida,
+        on_delete=models.CASCADE,
+        related_name="parcelas"
+    )
+    numero = models.PositiveIntegerField()
+    data_vencimento = models.DateField()
+    
